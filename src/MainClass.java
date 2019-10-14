5c746277485b9bcf7bdd4151d6a233ea1c83ff8f
edu-05 <edu-05@edu-01-HP>
2019-10-14 09:56:59 +0900

    계산기 숫자 클래스 전

---
+++ src/MainClass.java
@@ -0,0 +1,21 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		System.out.println("인터페이스를 해보자");
+		// 연산을 하는 프로그램
+		// 숫자가 필요 
+		// 최소 2개 이상
+		int num1 = 3;
+		int num2 = 2;
+		// 연산자 필요(+, -, *, /)
+//		num1 + num2;
+		// 계산해서 결과값 보여줌
+		int result = num1 + num2;
+		result = num1 - num2;
+		result = num1 * num2;
+		result = num1 / num2;
+	}
+
+}
